295845617917bb25326146a6f3646ec8ad20ec44
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-10-02 12:58:26 +0300

    Initial Commit

---
+++ InvestPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InvestPortfolio
+//
+//  Created by 19336088 on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
